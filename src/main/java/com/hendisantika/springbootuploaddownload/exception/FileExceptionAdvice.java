b6582a094c7fd5890ea60831acfb142bb5514c32
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-18 04:55:03 +0700

    #7 - Add FileExceptionAdvice

---
+++ src/main/java/com/hendisantika/springbootuploaddownload/exception/FileExceptionAdvice.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springbootuploaddownload.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-upload-download
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/10/20
+ * Time: 04.54
+ */
+@ControllerAdvice
+public class FileExceptionAdvice extends ResponseEntityExceptionHandler {
+
+}
