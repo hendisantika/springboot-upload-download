8c683d7061b51dcb52c2fba967bcc84e27f31dff
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-18 05:10:25 +0700

    #24 - Add uploadSingleFile API in FileController

--- src/main/java/com/hendisantika/springbootuploaddownload/controller/FileController.java
+++ src/main/java/com/hendisantika/springbootuploaddownload/controller/FileController.java
@@ -1,9 +1,16 @@
 package com.hendisantika.springbootuploaddownload.controller;
 
+import com.hendisantika.springbootuploaddownload.model.FileResponse;
 import com.hendisantika.springbootuploaddownload.service.IFileSystemStorage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,16 @@ public class FileController {
     @Autowired
     private IFileSystemStorage fileSystemStorage;
 
+    @PostMapping("/uploadfile")
+    public ResponseEntity<FileResponse> uploadSingleFile(@RequestParam("file") MultipartFile file) {
+        String upfile = fileSystemStorage.saveFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/download/")
+                .path(upfile)
+                .toUriString();
+
+        return ResponseEntity.status(HttpStatus.OK).body(new FileResponse(upfile, fileDownloadUri, "File uploaded " +
+                "with success!"));
+    }
 }
