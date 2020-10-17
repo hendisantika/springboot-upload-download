afe09761e3ea501e0912b518c17676acfb841032
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-18 05:03:14 +0700

    #17 - EnableConfigurationProperties in Main App

--- src/main/java/com/hendisantika/springbootuploaddownload/SpringbootUploadDownloadApplication.java
+++ src/main/java/com/hendisantika/springbootuploaddownload/SpringbootUploadDownloadApplication.java
@@ -1,9 +1,14 @@
 package com.hendisantika.springbootuploaddownload;
 
+import com.hendisantika.springbootuploaddownload.properties.FileUploadProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileUploadProperties.class
+})
 public class SpringbootUploadDownloadApplication {
 
     public static void main(String[] args) {
