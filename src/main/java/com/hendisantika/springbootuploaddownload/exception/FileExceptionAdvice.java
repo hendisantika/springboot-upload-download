d6d692b1967a97d9a5d5c175348989931791284b
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-18 04:55:44 +0700

    #9 - Add handleMaxSizeException in FileExceptionAdvice

--- src/main/java/com/hendisantika/springbootuploaddownload/exception/FileExceptionAdvice.java
+++ src/main/java/com/hendisantika/springbootuploaddownload/exception/FileExceptionAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.FileNotFoundException;
@@ -32,5 +33,16 @@ public class FileExceptionAdvice extends ResponseEntityExceptionHandler {
         ResponseError err = new ResponseError(LocalDateTime.now(), "File Not Found", details);
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Object> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+
+        List<String> details = new ArrayList<String>();
+        details.add(exc.getMessage());
+
+        ResponseError err = new ResponseError(LocalDateTime.now(), "File Size Exceeded", details);
+
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(err);
     }
 }
