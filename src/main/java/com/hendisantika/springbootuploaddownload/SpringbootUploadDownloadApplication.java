8ff509057bab57be73df4ead5784b0bb04591a8d
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-18 04:47:51 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootuploaddownload/SpringbootUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootuploaddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUploadDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUploadDownloadApplication.class, args);
+    }
+
+}
