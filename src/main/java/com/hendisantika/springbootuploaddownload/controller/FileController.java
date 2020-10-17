d1eb32552ed5217e0b70f8529d623988a416941d
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-18 05:11:50 +0700

    #25 - Add downloadFile API in FileController

--- src/main/java/com/hendisantika/springbootuploaddownload/controller/FileController.java
+++ src/main/java/com/hendisantika/springbootuploaddownload/controller/FileController.java
@@ -3,14 +3,22 @@ package com.hendisantika.springbootuploaddownload.controller;
 import com.hendisantika.springbootuploaddownload.model.FileResponse;
 import com.hendisantika.springbootuploaddownload.service.IFileSystemStorage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -39,5 +47,34 @@ public class FileController {
 
         return ResponseEntity.status(HttpStatus.OK).body(new FileResponse(upfile, fileDownloadUri, "File uploaded " +
                 "with success!"));
+    }
+
+    @PostMapping("/uploadfiles")
+    public ResponseEntity<List<FileResponse>> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        List<FileResponse> responses = Arrays.asList(files)
+                .stream()
+                .map(
+                        file -> {
+                            String upfile = fileSystemStorage.saveFile(file);
+                            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                                    .path("/api/download/")
+                                    .path(upfile)
+                                    .toUriString();
+                            return new FileResponse(upfile, fileDownloadUri, "File uploaded with success!");
+                        }
+                )
+                .collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(responses);
+    }
+
+    @GetMapping("/download/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+
+        Resource resource = fileSystemStorage.loadFile(filename);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
